b927443a892405b04cdc36f9404535fbf722a7a4
Alexis Jr. Banaag <aabanaag@gmail.com>
2022-11-28 17:22:21 +0800

    Initial commit

---
+++ baskin_robbins/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from baskin_robbins.users.models import User
+from baskin_robbins.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ baskin_robbins/users/tests/test_models.py
@@ -0,0 +1,5 @@
+from baskin_robbins.users.models import User
+
+
+def test_user_get_absolute_url(user: User):
+    assert user.get_absolute_url() == f"/users/{user.username}/"
