f8d6838e3a7c0c55dba84e382ec408a8d57e7f31
Alexis A. Banaag Jr <aabanaag@gmail.com>
2022-11-30 03:00:03 +0800

    feat: recipe (#16)

--- baskin_robbins/product/api/serializers.py
+++ baskin_robbins/product/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from baskin_robbins.branch.api.serializers import BranchSerializer
+from baskin_robbins.inventory.api.serializers import IngredientListRetrieveSerializer
 from baskin_robbins.product.models import Flavor, Product, Recipe, RecipeIngredient
 
 
@@ -34,6 +35,39 @@ class RecipeSerializer(serializers.ModelSerializer):
 
 
 class RecipeIngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecipeIngredient
+        fields = "__all__"
+        read_only_fields = ("id", "created_at", "updated_at")
+
+
+class RecipeCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = "__all__"
+
+
+class RecipeListRetrieveSerializer(serializers.ModelSerializer):
+    product = ProductListRetrieveSerializer(read_only=True)
+    recipe_ingredients = RecipeIngredientSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Recipe
+        fields = "__all__"
+        read_only_fields = ("id", "created_at", "updated_at")
+
+
+class RecipeIngredientCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RecipeIngredient
+        fields = "__all__"
+        read_only_fields = ("id", "created_at", "updated_at")
+
+
+class RecipeIngredientListRetrieveSerializer(serializers.ModelSerializer):
+    recipe = RecipeListRetrieveSerializer(read_only=True)
+    ingredient = IngredientListRetrieveSerializer(read_only=True)
+
     class Meta:
         model = RecipeIngredient
         fields = "__all__"
