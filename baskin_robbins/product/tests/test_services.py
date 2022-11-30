4c095e003f671154dcd92f72d7f6bd1bd2a29a85
Alexis Jr. Banaag <aabanaag@gmail.com>
2022-11-30 20:46:45 +0800

    feat: update test

--- baskin_robbins/product/tests/test_services.py
+++ baskin_robbins/product/tests/test_services.py
@@ -5,12 +5,14 @@ Product - service test
 from factory.faker import faker
 
 from baskin_robbins.conftest import BaskinRobbinsTestCase
+from baskin_robbins.inventory.models import Inventory
 from baskin_robbins.inventory.tests.factories import InventoryFactory
 from baskin_robbins.product.services import process_purchase
 from baskin_robbins.product.tests.factories import (
     ProductFactory,
     TransactionWithProductFactory,
 )
+from baskin_robbins.utils.exceptions import ProductNoInventory
 
 faker = faker.Faker()
 
@@ -21,7 +23,7 @@ class ProductServiceTestCase(BaskinRobbinsTestCase):
 
         self.product = ProductFactory.create(branch=self.main_branch)
 
-        InventoryFactory.create_batch(10, product=self.product)
+        InventoryFactory.create(product=self.product, quantity=10)
 
         self.transaction = TransactionWithProductFactory.create(product=self.product)
 
@@ -30,5 +32,15 @@ class ProductServiceTestCase(BaskinRobbinsTestCase):
     def test_process_purchase(self):
         process_purchase(self.product, 1)
 
-        print(self.product.inventory)
+        inventory = Inventory.objects.filter(product=self.product)
+
+        self.assertTrue(inventory.exists())
+        self.assertEqual(inventory.first().quantity, 9)
+
         self.assertEqual(self.product.transactions.count(), 2)
+
+    def test_purchase_should_fail(self):
+        with self.assertRaises(ProductNoInventory):
+            is_success = process_purchase(self.ice_cream, 1)
+
+            self.assertFalse(is_success)
